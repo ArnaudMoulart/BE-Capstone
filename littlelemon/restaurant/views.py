e370bf1ee99cb253728d2f95e544881b81f4c8ad
Arnaud Moulart <arnaud.moulart@gmail.Com>
2024-03-06 21:31:41 +0100

    modified:   restaurant/__pycache__/views.cpython-311.pyc 	modified:   restaurant/views.py 	modified:   templates/about.html 	modified:   templates/index.html

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-#from .models import Menu
+from .models import Menu
 
 # Create your views here.
 
@@ -8,8 +8,9 @@ def about(request):
     return render(request, "about.html", {'content': about_content})
 
 def menu(request):
-    about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."} 
-    return render(request, "menu.html", {'content': about_content})
+    menu_data = Menu.objects.all()
+    main_data = {"menu": menu_data}
+    return render(request, "menu.html", {'content': main_data})
 
 def home(request):
     return render(request, 'index.html',{})
